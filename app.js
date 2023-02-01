27e0af4197585aef5e98463055e09bd62b24d1ad
suraj <surajmishra9318@gmail.com>
2023-02-01 11:29:13 +0530

    register api and configuration

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const express = require('express')
+const express = require('express');
+const User = require('./db/User');
 const app = express();
+require('./db/config')
+app.use(express.json())
 
-app.get('/',(req,resp)=>{
-    resp.send('project started...')
+app.post('/register',async(req,resp)=>{
+    const user  = new User(req.body)
+    const result = await user.save();
+    resp.send(result)
 })
 app.listen(5000)
+
+
+
---
+++ db/User.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const userSchema = mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+})
+module.exports = mongoose.model('users',userSchema)

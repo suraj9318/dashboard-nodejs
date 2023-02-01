7f9cea9bdd473c897566823d6a853313edc24c35
suraj <surajmishra9318@gmail.com>
2023-02-01 21:44:42 +0530

    login api done

--- app.js
+++ app.js
@@ -7,8 +7,27 @@ app.use(express.json())
 app.use(cors())
 app.post('/register',async(req,resp)=>{
     const user  = new User(req.body)
-    const result = await user.save();
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
     resp.send(result)
+})
+
+app.post('/login',async(req,resp)=>{
+    let user = await User.find(req.body).select('-password');
+   if(req.body.email && req.body.password){
+    if(user.length > 0)
+    {
+        resp.send(user);
+    }
+    else{
+         resp.send({result : 'user not found'})
+    }
+   }
+   else{
+    resp.send({result : 'user not found'})
+}
+    
 })
 app.listen(5000)
 
